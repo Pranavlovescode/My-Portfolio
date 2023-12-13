69788db8d71146466837e1eb966406739c04a238
Pranavlovescode <pranavtitambe04@gmail.com>
2023-12-13 22:19:41 +0530

    Commitig the main code files

---
+++ server.js
@@ -0,0 +1,7 @@
+port = 3000
+const express = require('express')
+const app= express()
+app.use(express.static('public'));
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
